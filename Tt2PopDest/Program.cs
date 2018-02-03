3ad0295986f38aa29a1ec6749fc2d5ddb5b7ad9f
Adrian Sims <adriansims62781@gmail.com>
2018-02-03 13:08:46 +0000

    Small changes.

--- Tt2PopDest/Program.cs
+++ Tt2PopDest/Program.cs
@@ -4,11 +4,36 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Tt2PopDest
 {
     internal class DictUtils
     {
+        public static void AddEntryToList<T, U>(Dictionary<T, Dictionary<U, int>> d, T outerKey, U innerKey)
+        {
+            if (!d.TryGetValue(outerKey, out var innerDict))
+            {
+                // there was no inner dictionary for the outer key:
+                innerDict = new Dictionary<U, int>
+                {
+                    { innerKey, 1 }
+                };
+                d.Add(outerKey, innerDict);
+            }
+            else
+            {
+                // the outer key existed, but the inner dictionary doesn't contain this inner key:
+                if (!innerDict.ContainsKey(innerKey))
+                {
+                    innerDict.Add(innerKey, 1);
+                }
+                else
+                {
+                    innerDict[innerKey]++;
+                }
+            }
+        }
 
     }
     internal class Program
@@ -24,9 +49,22 @@ namespace Tt2PopDest
         }
         private static void Main(string[] args)
         {
+            var freqDict = new Dictionary<string, Dictionary<string, int>>();
+            //DictUtils.AddEntryToList(freqDict, "POO", "PKS");
+            //DictUtils.AddEntryToList(freqDict, "POO", "PKS");
+            //DictUtils.AddEntryToList(freqDict, "POO", "PKS");
+            //DictUtils.AddEntryToList(freqDict, "POO", "BSM");
+            //Environment.Exit(1);
             try
             {
-                var tipLocToCrs = new Dictionary<string, string>();
+                var stationfilename = "w:\\StationsRefData.xml";
+                var doc = XDocument.Load(stationfilename);
+                var tiplocToCrs = doc.Descendants("Station")
+                    .Select(x => new { Crs = x.Element("CRS")?.Value, Tiploc = x.Element("Tiploc")?.Value })
+                    .Where(x => !string.IsNullOrEmpty(x.Crs) && !string.IsNullOrEmpty(x.Tiploc))
+                    .ToDictionary(x => x.Tiploc, x => x.Crs);
+
+
                 var startTime = DateTime.Now;
                 var filename = "s:\\ttisf772.mca";
                 var count = 0;
@@ -37,7 +75,6 @@ namespace Tt2PopDest
                 var crsCompressor = new CrsCodec();
                 var minDate = DateTime.Today;
                 var maxDate = DateTime.Today;
-                var freqDict = new Dictionary<string, Dictionary<string, int>>();
                 foreach (var line in File.ReadLines(filename))
                 {
                     try
@@ -45,13 +82,17 @@ namespace Tt2PopDest
                         var recordType = line.Substring(0, 2);
                         if (line.Length > 2 && recordType == "TI")
                         {
-                            // this is a tiploc insert record, so store the mapping from Tiploc to CRS:
-                            var tiploc = line.Substring(2, 7);
-                            var crs = line.Substring(53, 3);
-                            if (crs.All(c => char.IsLetterOrDigit(c)))
-                            {
-                                tipLocToCrs.Add(tiploc, crs);
-                            }
+                            //// this is a tiploc insert record, so store the mapping from Tiploc to CRS:
+                            //var tiploc = line.Substring(2, 7);
+                            //var crs = line.Substring(53, 3);
+                            //if (crs.All(c => char.IsLetterOrDigit(c)))
+                            //{
+                            //    tiplocToCrs.Add(tiploc, crs);
+                            //    if (tiploc == "WATRLMN")
+                            //    {
+                            //        System.Diagnostics.Debug.WriteLine($"");
+                            //    }
+                            //}
                         }
                         else if (recordType == "BS")
                         {
@@ -72,8 +113,8 @@ namespace Tt2PopDest
                                 if (!(recordType == "LI" && line.Substring(15, 4).All(char.IsWhiteSpace)))
                                 {
                                     // the record is not a "station passing" record:
-                                    var tiploc = line.Substring(2, 7);
-                                    var crs = GetValueOrNull(tipLocToCrs, tiploc);
+                                    var tiploc = line.Substring(2, 7).Trim();
+                                    var crs = GetValueOrNull(tiplocToCrs, tiploc);
                                     if (crs != null)
                                     {
                                         oneRun.Add(crs);
@@ -95,7 +136,7 @@ namespace Tt2PopDest
                         Console.WriteLine($"Error at line {linenumber + 1} : {e.Message}");
                     }
                     linenumber++;
-                    if (linenumber % 1000 == 999)
+                    if (linenumber % 1000000 == 999999)
                     {
                         Console.WriteLine($"{linenumber + 1}");
                     }
@@ -105,12 +146,22 @@ namespace Tt2PopDest
                 Console.WriteLine($"count of train runs is {allRuns.Count()} - time was {timetaken.TotalSeconds} seconds");
                 foreach (var run in allRuns)
                 {
-                    for (int i = 0; i < run.Count - 1; i++)
+                    for (int origin = 0; origin < run.Count - 1; origin++)
                     {
-                        for (int j = i + 1; j < count; j++)
+                        for (int destination = origin + 1; destination < run.Count; destination++)
                         {
-
+                            DictUtils.AddEntryToList(freqDict, run[origin], run[destination]);
                         }
+                    }
+                }
+                Console.WriteLine($"Frequency dictionary size is {freqDict.Count}");
+                foreach (var entry in freqDict)
+                {
+                    Console.WriteLine($"--- {entry.Key} ---");
+                    var top5 = entry.Value.OrderByDescending(x => x.Value);
+                    foreach (var top in top5)
+                    {
+                        Console.WriteLine($"    {top.Key} {top.Value}");
                     }
                 }
             }
